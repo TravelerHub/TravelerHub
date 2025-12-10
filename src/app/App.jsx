e7bfafab129223a5a1788828ee590915cd1ee1d1
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-09 18:03:29 -0800

    Finish set up database

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
+
 import LandingPage from "./pages/LandingPage.jsx";
 import AboutUsPage from "./pages/AboutUsPage.jsx";
 import ContactUsPage from "./pages/ContactUsPage.jsx";
 import ServicePage from "./pages/ServicePage.jsx";
 import FeedbackPage from "./pages/FeedbackPage.jsx";
+
+const supabase = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_ANON_KEY
+);
 
 function App() {
   return (
