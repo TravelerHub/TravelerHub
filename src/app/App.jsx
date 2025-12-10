5d07b60ca7f9631458c057b59f0820da4d0dc6d9
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-09 17:29:50 -0800

    Add some pages and navigation

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.jsx";
+import AboutUsPage from "./pages/AboutUsPage.jsx";
+import ContactUsPage from "./pages/ContactUsPage.jsx";
+import ServicePage from "./pages/ServicePage.jsx";
+import FeedbackPage from "./pages/FeedbackPage.jsx";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/AboutUsPage" element={<AboutUsPage />} />
+        <Route path="/ContactUsPage" element={<ContactUsPage />} />
+        <Route path="/ServicePage" element={<ServicePage />} />
+        <Route path="/FeedbackPage" element={<FeedbackPage />} />
+      </Routes>
+    </Router>
   );
 }
 
