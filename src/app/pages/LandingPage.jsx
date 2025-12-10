3673283f441fbb8dd7ca4db36f1af7e341bb8e2c
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-09 16:44:01 -0800

    Start from here

---
+++ src/app/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+  return (
+    <nav>
+      <button>About Us</button>
+      <button>Services</button>
+      <button>Feedback</button>
+      <button>Contact Us</button>
+      <button>Log in / Sign up</button>
+    </nav>
+  );
+}
