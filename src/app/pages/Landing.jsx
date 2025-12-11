e61f3e85d571d228bf21cd95fe432d6697c59ebc
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-10 22:33:07 -0800

    Fix the naming to follow standard

--- src/app/pages/Landing.jsx
+++ src/app/pages/Landing.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
 
   return (
     <nav>
-      <button onClick={() => navigate('AboutUsPage')}>About Us</button>
-      <button onClick={() => navigate('ServicePage')}>Service</button>
-      <button onClick={() => navigate('FeedbackPage')}>Feedback</button>
-      <button onClick={() => navigate('ContactUsPage')}>Contact Us</button>
+      <button onClick={() => navigate("about")}>About Us</button>
+      <button onClick={() => navigate("service")}>Service</button>
+      <button onClick={() => navigate("feedback")}>Feedback</button>
+      <button onClick={() => navigate("contactus")}>Contact Us</button>
       <button>Log in / Sign up</button>
     </nav>
   );
