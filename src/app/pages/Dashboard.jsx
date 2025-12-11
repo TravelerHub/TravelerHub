2736415ebfa2017a0e1ce7030ff3960f6a1b7727
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-11 01:12:27 -0800

    Add log out button and install bcrypt dependency

--- src/app/pages/Dashboard.jsx
+++ src/app/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
+  const navigate = useNavigate();
+
   return (
     <div>
         <h1>Dashboard</h1>
+        <button onClick={() => navigate("/")}>Log out</button>
     </div>
   );
 }
