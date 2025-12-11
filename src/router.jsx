b9124f3e6ff13f2d9ba24180519a64cbcbcc13e9
PhuocWinQuy <phuocquyforwork@gmail.com>
2025-12-10 23:47:53 -0800

    Create router separatly and supabase client

---
+++ src/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Landing from "./app/pages/Landing.jsx";
+import About from "./app/pages/About.jsx";
+import ContactUs from "./app/pages/ContactUs.jsx";
+import Service from "./app/pages/Service.jsx";
+import Feedback from "./app/pages/Feedback.jsx";
+import Login from "./app/pages/Login.jsx";
+import SignUp from "./app/pages/SignUp.jsx";
+import Dashboard from "./app/pages/Dashboard.jsx";
+
+const router = createBrowserRouter([
+    { path: "/", element: <Landing /> },
+    { path: "/about", element: <About /> },
+    { path: "/contactus", element: <ContactUs /> },
+    { path: "/service", element: <Service /> },
+    { path: "/feedback", element: <Feedback /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <SignUp /> },
+    { path: "/dashboard", element: <Dashboard /> },
+]);
+
+export default router;
