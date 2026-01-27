6ef0bd0ab4c23752ff99443a9d7a4d4f33c23799
MyLu004 <myluwork004@gmail.com>
2026-01-27 15:03:50 -0800

    add the navigation folder for multiple nav in the app

--- frontend/src/app/pages/Landing.jsx
+++ frontend/src/app/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/Footer";
 
 function Landing() {
