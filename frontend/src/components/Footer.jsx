846da3f274531c1b84221cedc29372c42b267608
MyLu004 <myluwork004@gmail.com>
2026-02-04 08:50:55 -0800

    finish styling loging page to match with the design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
 
   return (
     <footer className="bg-gray-900 text-white">
-      <div className="flex justify-center align-center w-screen px6 py-4">
-        {/* Main Footer Content */}
-        <div className="flex justify-in-between space-x-5 gap-6 mb-8">
+      <div className="flex justify-center align-center w-screen px-6 py-4">
+        {/* Main Footer Content */}-
+        <div className="flex justify-in-between space-x-5 gap-6">
           <Link to="/about" className="footer-btn">
             About Us
           </Link>
