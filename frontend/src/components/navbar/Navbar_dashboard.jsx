7109be36edb85b9f0afc242ff6bad3f3a429d6e8
PhuocWinQuy <108718527+Phuoc-Quy@users.noreply.github.com>
2026-02-06 23:09:01 -0800

    Add Message page

--- frontend/src/components/navbar/Navbar_dashboard.jsx
+++ frontend/src/components/navbar/Navbar_dashboard.jsx
@@ -77,7 +77,7 @@ function Navbar_Dashboard() {
           {/* Messenger */}
           <div className="relative group">
             <button
-              onClick={() => navigate("/")}
+              onClick={() => navigate("/message")}
               className="w-10 h-10 flex items-center justify-center rounded-lg hover:bg-gray-100 transition"
             >
               <span className="text-lg">💬</span>
